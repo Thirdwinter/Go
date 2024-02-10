a0c99cde33a427170ddb9719d680ddca0727daa5
Thirdwinter <zzx15601035637@outlook.com>
2024-02-10 23:37:10 +0800

    还是测试

--- go-demo/main.go
+++ go-demo/main.go
@@ -1,7 +1,14 @@
 package main
-import(
-	"fmt"
+
+import (
+	"github.com/gin-gonic/gin"
 )
-func main(){
-	fmt.Println("这是测试")
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"msg": "hello gin",
+		})
+	})
 }
