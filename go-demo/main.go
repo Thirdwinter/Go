ca4b065530823c87352277b229a5637b9eec0995
Thirdwinter <zzx15601035637@outlook.com>
2024-02-10 23:57:33 +0800

    一次普通暂存

--- go-demo/main.go
+++ go-demo/main.go
@@ -6,9 +6,26 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(ctx *gin.Context) {
+	r.GET("/api/auth/register", func(ctx *gin.Context) {
+		// 获取参数
+		name:=ctx.PostForm("name")
+		phonenumber:=ctx.PostForm("phonenumber")
+		password:=ctx.PostForm("password")
+
+
+		// 数据验证
+
+
+		// 判断手机号是否存在
+
+
+		// 创建用户
+
+
+		// 返回结果
 		ctx.JSON(200, gin.H{
-			"msg": "hello gin",
+			"msg": "注册成功",
 		})
 	})
+	panic(r.Run())
 }
