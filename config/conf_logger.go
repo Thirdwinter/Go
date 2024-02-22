e57ab8879ca4f65cadb6a486dc445c1c50b880f9
Thirdwinter <zzx15601035637@outlook.com>
2024-02-22 23:10:51 +0800

    1111

--- config/conf_logger.go
+++
@@ -1,9 +0,0 @@
-package config
-
-type Logger struct {
-	Out   bool   `yaml:"out"`
-	Level string `yaml:"loglevel"`
-	Path  string `yaml:"logpath"`
-	Name  string `yaml:"logname"`
-	Size  int64  `yaml:"logsize"`
-}
--- config/enter.go
+++
@@ -1,8 +0,0 @@
-// !config包存放配置信息
-package config
-
-type Config struct {
-	Mysql  Mysql  `yaml:"mysql"`
-	Logger Logger `yaml:"logger"`
-	System System `yaml:"system"`
-}
